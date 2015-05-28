0bc756d3c63f1e10a4681847dc68fade9a599ad3
Satriya <Satriya@TDG-Satriya>
2015-05-28 13:50:36 +0800

    Programming puzzle by Bornevia solved. Commited is a working solution written in Java.

--- src/org/code/bornevia/CustomersCSV.java
+++ src/org/code/bornevia/CustomersCSV.java
@@ -6,31 +6,95 @@
 package org.code.bornevia;
 
 import java.io.BufferedReader;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
 import java.io.IOException;
-import java.nio.charset.Charset;
-import java.nio.file.FileSystems;
-import java.nio.file.Files;
-import java.nio.file.Path;
 import java.util.HashMap;
 import java.util.Map;
+import java.util.TreeMap;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 
 /**
  *
  * @author Satriya
  */
 public class CustomersCSV {
-    private Map customers = new HashMap();
+    private Map<String, Properties> customers = new HashMap<>();
+    private String path, regex;
     
-    private void readCSV(String path, String fName) {
-        Charset charset = Charset.forName("US-ASCII");
-        Path path = FileSystems.getDefault().getPath(path, fName);
-        try (BufferedReader reader = Files.newBufferedReader(path, charset)) {
-            String line = null;
-            while ((line = reader.readLine()) != null) {
-                System.out.println(line);
+    public CustomersCSV (String path, String regex) {
+        this.path = path;
+        this.regex = regex;
+        this.rebuild(path, regex);
+    }
+    
+    private void rebuild(String path, String regex) {     
+        try(BufferedReader br = new BufferedReader(new FileReader(path))) {
+            String line = br.readLine();
+            
+            while (line != null) {
+                if (this.isEntry(line)) {
+                    String[] arr = line.split(regex);
+                    String[] entries = new String[4];
+
+                    for (int i = 0; i < entries.length; i++) {
+                        entries[i] = i < arr.length ? arr[i] : "";
+                    }
+
+                    if (this.customers.isEmpty() || !this.customers.containsKey(entries[0])) {
+                        this.updateDict(entries);
+                    } else {
+                        Properties properties = this.customers.get(entries[0]);
+                        entries[1] = properties.getEmail().isEmpty() ? entries[1] : properties.getEmail();
+                        entries[2] = properties.getFacebook().isEmpty() ? entries[2] : properties.getFacebook();
+                        entries[3] = properties.getTwitter().isEmpty() ? entries[3] : properties.getTwitter();
+                        this.updateDict(entries);
+                    }
+                }
+                line = br.readLine();
             }
-        } catch (IOException x) {
-            System.err.format("IOException: %s%n", x);
+            
+            customers = new TreeMap<>(customers);
+        } catch (FileNotFoundException ex) {
+            Logger.getLogger(CustomersCSV.class.getName()).log(Level.SEVERE, null, ex);
+        } catch (IOException ex) {
+            Logger.getLogger(CustomersCSV.class.getName()).log(Level.SEVERE, null, ex);
         }
+    }
+    
+    private boolean isEntry(String line) {
+        if (line.contains("*****")) {
+            return false;
+        } else 
+            return !line.contains("Name,");
+    }
+    
+    private void updateDict(String[] entries) {
+        this.customers.put(
+            entries[0], 
+            new Properties.Builder()
+                .email(entries[1])
+                .facebook(entries[2])
+                .twitter(entries[3]).build()
+        );
+    }
+    
+    public String getCustomers() {
+        StringBuilder sb = new StringBuilder();
+                
+        for (Map.Entry<String, Properties> e : this.customers.entrySet()) {
+            sb.append(e.getKey());
+            sb.append(regex);
+            Properties p = e.getValue();
+            sb.append(p.getEmail());
+            sb.append(regex);
+            sb.append(p.getFacebook());
+            sb.append(regex);
+            sb.append(p.getTwitter());
+            sb.append(System.lineSeparator());
+        }
+        
+        return sb.toString();
     }
 }
---
+++ src/org/code/bornevia/Main.java
@@ -0,0 +1,40 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package org.code.bornevia;
+
+import java.io.File;
+import javax.swing.JFileChooser;
+
+/**
+ *
+ * @author Satriya
+ */
+public class Main {
+    public static void main(String args[]) {
+        JFileChooser fileChooser = new JFileChooser();
+        int result;
+        File file = null;
+        
+        fileChooser.setCurrentDirectory(new File(System.getProperty("user.home")));
+        result = fileChooser.showOpenDialog(fileChooser);
+        
+        if (result == JFileChooser.APPROVE_OPTION) {
+            file = fileChooser.getSelectedFile();
+        }
+        
+        CustomersCSV customers = new CustomersCSV(file.getAbsolutePath(), ",");
+        StringBuilder sb = new StringBuilder();
+        sb.append("/************************** customers_merged.csv ***************************/");
+        sb.append(System.lineSeparator());
+        sb.append("/****************************** Start ******************************/");
+        sb.append(System.lineSeparator());
+        sb.append("Name,Email,Facebook,Twitter");
+        sb.append(System.lineSeparator());
+        sb.append(customers.getCustomers());
+        sb.append("/******************************* End *******************************/");
+        System.out.println(sb);
+    }
+}
